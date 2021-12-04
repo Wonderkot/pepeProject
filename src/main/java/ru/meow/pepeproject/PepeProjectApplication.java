d0eb0d3ea878de658d841d75341d1fc8059e8a9f
ybogdanov <79097146710@ya.ru>
2021-12-04 21:11:17 +0400

    Initial commit

---
+++ src/main/java/ru/meow/pepeproject/PepeProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.meow.pepeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PepeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/meow/pepeproject/PepeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.meow.pepeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PepeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
