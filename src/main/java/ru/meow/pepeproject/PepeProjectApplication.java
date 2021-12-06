0ae20e50ee518e2f207c8fe60a5a8a68274890de
ybogdanov <79097146710@yandex.ru>
2021-12-06 11:42:55 +0400

    очистка папки с пикчами, проперть

--- src/main/java/ru/meow/pepeproject/PepeProjectApplication.java
+++ src/main/java/ru/meow/pepeproject/PepeProjectApplication.java
@@ -1,13 +1,35 @@
 package ru.meow.pepeproject;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.FileSystemUtils;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @SpringBootApplication
 public class PepeProjectApplication {
 
+    @Value("${cleanOnStart}")
+    private boolean preClean;
+
     public static void main(String[] args) {
         SpringApplication.run(PepeProjectApplication.class, args);
+    }
+
+    @PostConstruct
+    private void clean() {
+        if (preClean) {
+            Path path = Path.of("./", "img");
+            try {
+                FileSystemUtils.deleteRecursively(path.toFile());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
